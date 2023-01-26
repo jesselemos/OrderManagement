f0db88106b11742cc446db211f374f2fcde8a11b
Jesse Lemos <jesselemos@gmail.com>
2023-01-26 15:57:50 +0000

    Adding tests for UpdateOrderAddress Command handlers and validators

---
+++ OrderService.API.UnitTests/Commands/UpdateOrderAddress/UpdateOrderAddressCommandHandlerTests.cs
@@ -0,0 +1,67 @@
+using AutoMapper;
+using NSubstitute;
+using OrderService.API.Commands.UpdateOrderAddress;
+using OrderService.API.Repositories;
+using OrderService.API.UnitTests.Helpers;
+
+namespace OrderService.API.UnitTests.Commands.UpdateOrderAddress
+{
+    [TestFixture]
+    public class UpdateOrderAddressCommandHandlerTests
+    {
+        private IOrderRepository _orderRepository;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = AutoMapperHelper.CreateMapper();
+            _orderRepository = new OrderRepository(DatabaseHelper.GetOrderDbContext());
+         }
+
+        [Test]
+        public async Task CanUpdateOrderAddressAndSetStatusCreated()
+        {
+            var result = await new UpdateOrderAddressCommandHandler(_orderRepository, _mapper).Handle(
+                new UpdateOrderAddressCommand()
+                {
+                    OrderId = DatabaseHelper.OrderSeedId,
+                    AddressLine = "New AddressLine",
+                    AddressName = "New AddressName",
+                    EirCode = "NewCode",
+                    County = "New County",
+                }, new CancellationToken());
+
+            var order = await _orderRepository.GetOrderByIdAsync(DatabaseHelper.OrderSeedId);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(order, Is.Not.Null);
+                Assert.That(order?.AddressLine == "New AddressLine");
+                Assert.That(order?.AddressName == "New AddressName");
+                Assert.That(order?.EirCode == "NewCode");
+                Assert.That(order?.County == "New County");
+            });
+        }
+
+        [Test]
+        public void ThrowExceptionIfOrderNotExistsInDatabase()
+        {
+            var commandHandler = new UpdateOrderAddressCommandHandler(Substitute.For<IOrderRepository>(), _mapper);
+            Guid orderId = new();
+
+            Assert.That(async () =>
+                await commandHandler.Handle(
+                    new UpdateOrderAddressCommand()
+                    {
+                        OrderId = orderId,
+                        AddressLine = "AddressLine",
+                        AddressName = "AddressName",
+                        EirCode = "EirCode",
+                        County = "County",
+                    }, new CancellationToken()),
+                        Throws.TypeOf<Exception>()
+                        .With.Message.EqualTo($"OrderId: {orderId} not found in our database."));
+        }
+    }
+}
---
+++ OrderService.API.UnitTests/Commands/UpdateOrderAddress/UpdateOrderAddressCommandValidatorTests.cs
@@ -0,0 +1,70 @@
+﻿using OrderService.API.Commands.UpdateOrderAddress;
+
+namespace OrderService.API.UnitTests.Commands.UpdateOrderAddress
+{
+    [TestFixture]
+    public class UpdateOrderAddressCommandValidatorTests
+    {
+        [Test]
+        public async Task UpdateOrderAddressCommandValidatorIsValid()
+        {
+            var validator = new UpdateOrderAddressCommandValidator();
+            var command = new UpdateOrderAddressCommand()
+            {
+                OrderId = Guid.NewGuid(),
+                AddressLine = "AddressLine",
+                AddressName = "AddressName",
+                EirCode = "EirCode",
+                County = "County",
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.IsValid);
+                Assert.That(!result.Errors.Any());
+            });
+        }
+
+        [TestCase("OrderId")]
+        [TestCase("AddressLine")]
+        [TestCase("AddressName")]
+        [TestCase("EirCode")]
+        [TestCase("County")]
+        public async Task UpdateOrderAddressCommandValidatorContainsErrorMessageForRequiredFields(string field)
+        {
+            var result = await new UpdateOrderAddressCommandValidator().ValidateAsync(new UpdateOrderAddressCommand());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(!result.IsValid);
+                Assert.That(result.Errors.Any(a => a.ErrorMessage == $"{{{field}}} is required."));
+            });
+        }
+
+        [TestCase("AddressLine", 200)]
+        [TestCase("AddressName", 50)]
+        [TestCase("EirCode", 7)]
+        [TestCase("County", 20)]
+        public async Task UpdateOrderAddressCommandValidatorContainsErrorMessageForExceedCharacters(string field, int maxLength)
+        {
+            var validator = new UpdateOrderAddressCommandValidator();
+            var command = new UpdateOrderAddressCommand()
+            {
+                AddressLine = "AddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineAddressLineddressLineAddressLineAddressLineAddressLineAddressLine",
+                AddressName = "AddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressNameAddressName",
+                EirCode = "EirCodeEirCode",
+                County = "CountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCountyCounty",
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(!result.IsValid);
+                Assert.That(result.Errors.Any(a => a.ErrorMessage == $"{{{field}}} must not exceed {maxLength} characters."));
+            });
+        }
+    }
+}
