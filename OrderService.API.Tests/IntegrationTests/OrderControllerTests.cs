627f6b87fe1dbafcab8277170986949522979f91
Jesse Lemos <jesselemos@gmail.com>
2023-01-26 22:54:20 +0000

    Adding Support to Integration Tests

--- OrderService.API.Tests/IntegrationTests/OrderControllerTests.cs
+++ OrderService.API.Tests/IntegrationTests/OrderControllerTests.cs
@@ -1,6 +1,30 @@
-﻿namespace OrderService.API.Tests.IntegrationTests
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using OrderService.API.Tests.UnitTests.Helpers;
+
+namespace OrderService.API.Tests.IntegrationTests
 {
-    internal class OrderControllerTests
+    [TestFixture]
+    public class OrderControllerTests
     {
+        private HttpClient httpClient;
+
+        [SetUp]
+        public void Init()
+        {
+            httpClient = new WebApplicationFactory<Program>().CreateClient();
+
+            DatabaseHelper.GetOrderDbContext();
+        }
+
+        [Test]
+        public async Task GetEntityRowReturnsForbiddenWhenViewPermissionNotPresent()
+        {
+            // Act
+            var result = await httpClient.GetAsync($"/api/v1/Order/{DatabaseHelper.OrderSeedId}");
+
+            //assert
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
     }
 }
