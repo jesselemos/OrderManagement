f2ce8e0acaccede7a35edd21e4accbb245bafa51
Jesse Lemos <jesselemos@gmail.com>
2023-01-26 15:45:30 +0000

    Adding unit tests for CancelOrder Notification and Validation

---
+++ OrderService.API.UnitTests/Commands/CancelOrder/CancelOrderCommandValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using OrderService.API.Commands.CancelOrder;
+
+namespace OrderService.API.UnitTests.Commands.CancelOrder
+{
+    [TestFixture]
+    public class CancelOrderCommandValidatorTests
+    {
+        [Test]
+        public async Task CancelOrderCommandValidatorIsValid()
+        {
+            var validator = new CancelOrderCommandValidator();
+            var command = new CancelOrderCommand()
+            {
+                OrderId = Guid.NewGuid()
+            };
+
+            var result = await validator.ValidateAsync(command);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.IsValid);
+                Assert.That(!result.Errors.Any());
+            });
+        }
+
+        [TestCase("OrderId")]
+        public async Task CancelOrderCommandValidatorContainsErrorMessageForRequiredFields(string field)
+        {
+            var result = await new CancelOrderCommandValidator().ValidateAsync(new CancelOrderCommand());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(!result.IsValid);
+                Assert.That(result.Errors.Any(a => a.ErrorMessage == $"{{{field}}} is required."));
+            });
+        }
+    }
+}
---
+++ OrderService.API.UnitTests/Commands/CancelOrder/CancelOrderNotificationHandlerTests.cs
@@ -0,0 +1,58 @@
+using NSubstitute;
+using OrderService.API.Commands.CancelOrder;
+using OrderService.API.Repositories;
+using OrderService.API.UnitTests.Helpers;
+
+namespace OrderService.API.UnitTests.Commands.CancelOrder
+{
+    [TestFixture]
+    public class CancelOrderNotificationHandlerTests
+    {
+        private IOrderRepository _orderRepository;
+        private IProductRepository _productRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            var orderDbContext = DatabaseHelper.GetOrderDbContext();
+            _orderRepository = new OrderRepository(orderDbContext);
+            _productRepository = new ProductRepository(orderDbContext);
+        }
+
+        [Test]
+        public async Task CanCancelOrderNotificationAndProductStockIsDecreased()
+        {
+            var productId = DatabaseHelper.ProductSeedId;
+            var product = await _productRepository.GetProductByIdAsync(productId);
+            var oldStock = product?.Stock;
+            await new CancelOrderNotificationHandler(_orderRepository, _productRepository).Handle(
+                new CancelOrderNotification
+                (new CancelOrderCommand()
+                {
+                    OrderId = DatabaseHelper.OrderSeedId,
+                }), new CancellationToken());
+
+            var order = await _orderRepository.GetOrderByIdAsync(DatabaseHelper.OrderSeedId);
+            var prodQuantity = order?.OrderItems.Find(f => f.Product?.Id == productId)?.Quantity;
+
+            Assert.That(product?.Stock, Is.EqualTo(oldStock + prodQuantity));
+        }
+
+        [Test]
+        public void ThrowExceptionIfOrderNotExistsInDatabase()
+        {
+            var handler = new CancelOrderNotificationHandler(_orderRepository, Substitute.For<IProductRepository>());
+            Guid orderId = new();
+
+            Assert.That(async () =>
+                await handler.Handle(
+                    new CancelOrderNotification(
+                        new CancelOrderCommand()
+                        {
+                            OrderId = orderId,
+                        }), new CancellationToken()),
+                        Throws.TypeOf<Exception>()
+                        .With.Message.EqualTo($"OrderId: {orderId} not found in our database."));
+        }
+    }
+}
