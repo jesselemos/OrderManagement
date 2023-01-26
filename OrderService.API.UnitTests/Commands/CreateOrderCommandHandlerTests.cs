0c6380809933790274912e3fad0880e0fc6faef2
Jesse Lemos <jesselemos@gmail.com>
2023-01-26 12:53:25 +0000

    Adding unit tests for CreateOrderCommandHandlerTests

--- OrderService.API.UnitTests/Commands/CreateOrderCommandHandlerTests.cs
+++ OrderService.API.UnitTests/Commands/CreateOrderCommandHandlerTests.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using NSubstitute;
 using OrderService.API.Commands.CreateOrder;
+using OrderService.API.Entities;
+using OrderService.API.Models;
 using OrderService.API.Repositories;
 using OrderService.API.UnitTests.Helpers;
 
@@ -10,6 +13,8 @@ namespace OrderService.API.UnitTests
     {
         public DbContextOptions<OrderDbContext> _orderDbContextOptions;
         private OrderDbContext _orderDbContext;
+        private IOrderRepository _orderRepository;
+        private IProductRepository _productRepository;
         private IMapper _mapper;
 
         [SetUp]
@@ -22,16 +27,22 @@ namespace OrderService.API.UnitTests
 
             _orderDbContext = new OrderDbContext(_orderDbContextOptions);
 
+            _orderDbContext.Database.EnsureDeleted();
+            _orderDbContext.Database.EnsureCreated();
+
+            _orderDbContext.Products.Add(new Product { Id = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"), Name = "Brownie", Price = 6, Stock = 10 });
+
+            _orderDbContext.SaveChangesAsync();
+
             _mapper = AutoMapperHelper.CreateMapper();
+            _orderRepository = new OrderRepository(_orderDbContext);
+            _productRepository = new ProductRepository(_orderDbContext);
         }
 
         [Test]
-        public void Test1()
+        public async Task CanCreateOrder()
         {
-            var orderRepository = new OrderRepository(_orderDbContext);
-            var productRepository = new ProductRepository(_orderDbContext);
-
-            var obj = new CreateOrderCommandHandler(orderRepository, productRepository, _mapper).Handle(
+            var result = await new CreateOrderCommandHandler(_orderRepository, _productRepository, _mapper).Handle(
                 new CreateOrderCommand()
                 {
                     CustomerName = "Name",
@@ -39,21 +50,104 @@ namespace OrderService.API.UnitTests
                     AddressName = "AddressName",
                     EirCode = "EirCode",
                     County = "County",
-                    OrderItems = new List<Models.CreateOrderItem>
+                    OrderItems = new List<CreateOrderItem>
                     {
-                        new Models.CreateOrderItem
+                        new CreateOrderItem
                         {
                             ProductId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
                             Quantity = 10
                         }
                     }
-
-                    //TotalPrice = 10,
-                    //ProductId = Guid.NewGuid()
                 }, new CancellationToken());
 
-            Assert.That(obj, Is.Not.Null);
-            Assert.Pass();
+            Assert.That(Guid.TryParse(result.ToString(), out Guid guidResult), Is.True);
+        }
+
+        [Test]
+        public void ThrowExceptionIfProductNotExistsInDatabase()
+        {
+            var commandHandler = new CreateOrderCommandHandler(_orderRepository, Substitute.For<IProductRepository>(), _mapper);
+            Guid productId = new();
+
+            Assert.That(async () => 
+                await commandHandler.Handle(
+                    new CreateOrderCommand()
+                    {
+                        CustomerName = "Name",
+                        AddressLine = "AddressLine",
+                        AddressName = "AddressName",
+                        EirCode = "EirCode",
+                        County = "County",
+                        OrderItems = new List<CreateOrderItem>
+                        {
+                            new CreateOrderItem
+                            {
+                                ProductId = productId,
+                                Quantity = 10
+                            }
+                        }
+                    }, new CancellationToken()),
+                        Throws.TypeOf<Exception>()
+                        .With.Message.EqualTo($"ProductId: {productId} not found in our database"));
+        }
+
+        [Test]
+        public async Task ThrowExceptionIfThereIsNotEnoughStockForTheProductAsync()
+        {
+            var commandHandler = new CreateOrderCommandHandler(_orderRepository, _productRepository, _mapper);
+            var productId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+            var product = await _productRepository.GetProductByIdAsync(productId);
+
+            Assert.That(async () =>
+                    await commandHandler.Handle(
+                        new CreateOrderCommand()
+                        {
+                            CustomerName = "Name",
+                            AddressLine = "AddressLine",
+                            AddressName = "AddressName",
+                            EirCode = "EirCode",
+                            County = "County",
+                            OrderItems = new List<CreateOrderItem>
+                            {
+                            new CreateOrderItem
+                            {
+                                ProductId = productId,
+                                Quantity = 999
+                            }
+                            }
+                        }, new CancellationToken()),
+                        Throws.TypeOf<Exception>()
+                        .With.Message.EqualTo($"There is only {product.Stock} units available of {product.Name}"));
+        }
+
+        [Test]
+        public void OrderValidation()
+        {
+            var orderRepository = new OrderRepository(_orderDbContext);
+            var productRepository = new ProductRepository(_orderDbContext);
+
+            var obj = new CreateOrderCommandHandler(orderRepository, productRepository, _mapper).Handle(
+                new CreateOrderCommand()
+                {
+                    CustomerName = "",
+                    AddressName = "AddressName",
+                    EirCode = "EirCode",
+                    County = "County",
+                    OrderItems = new List<CreateOrderItem>
+                    {
+                        new CreateOrderItem
+                        {
+                            ProductId = Guid.Parse("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+                            Quantity = 10
+                        }
+                    }
+                }, new CancellationToken());
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(obj.IsCompleted, Is.True);
+                Assert.That(obj, Is.Not.Null);
+            });
         }
     }
 }
